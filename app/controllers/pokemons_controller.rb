ca5c797447b56dd6135354189e0659a53e3393d9
emma puget <puget.emma@gmail.com>
2024-02-28 13:08:08 +0100

    route + view card
    Signed-off-by: emma puget <puget.emma@gmail.com>

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,5 +1,6 @@
 class PokemonsController < ApplicationController
   before_action :find_pokemon, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @pokemons = Pokemon.all
